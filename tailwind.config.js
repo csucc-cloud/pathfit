932a21601b239b745f88ed853590866074a027c5
csucc-cloud <sialongodavie68@gmail.com>
2026-04-14 16:52:57 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fb-blue': '#039be5',
+        'fb-navy': '#051e34',
+      },
+    },
+  },
+  plugins: [],
+};
