19a5bc9d306f198dd9ba5f091130016c942be756
csucc-cloud <sialongodavie68@gmail.com>
2026-04-15 13:49:17 +0800

    Create exercises.js

---
+++ src/constants/exercises.js
@@ -0,0 +1,18 @@
+// src/constants/exercises.js
+export const PATHFIT_EXERCISES = [
+  { id: 'ex1', name: "Svend Press", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex2', name: "Floor Dumbbell Fly", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex3', name: "Downward Dog Push Up", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex4', name: "Aquaman", reps: 60, sets: 2, unit: "secs" },
+  { id: 'ex5', name: "Superman", reps: 60, sets: 2, unit: "secs" },
+  { id: 'ex6', name: "Hip Hinge", reps: 60, sets: 2, unit: "secs" },
+  { id: 'ex7', name: "Sub for Bench Dips", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex8', name: "Push-up", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex9', name: "Plank Taps", reps: 10, sets: 3, unit: "reps" },
+  { id: 'ex10', name: "Low Squat", reps: 10, sets: 3, unit: "reps" },
+  { id: 'ex11', name: "Static Lunge", reps: 60, sets: 2, unit: "secs" },
+  { id: 'ex12', name: "Wall Sit", reps: 60, sets: 3, unit: "secs" },
+  { id: 'ex13', name: "Dumbbell Fly", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex14', name: "Reverse Fly", reps: 8, sets: 3, unit: "reps" },
+  { id: 'ex15', name: "Dumbbell Push Press", reps: 8, sets: 3, unit: "reps" },
+];
