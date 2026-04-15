6d32f088237f79168c10ca83734e085fe1810437
csucc-cloud <sialongodavie68@gmail.com>
2026-04-15 13:18:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'fb-blue': '#039be5',
-        'fb-navy': '#051e34',
+        fbOrange: '#f57c00',    // Primary Action Orange
+        fbAmber: '#ffca28',     // Light Accent Orange
+        fbNavy: '#051e34',      // Sidebar & Depth
+        fbGray: '#f4f7f9',      // App Background
       },
     },
   },
