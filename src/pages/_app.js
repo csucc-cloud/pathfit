38b1986229362c628c6b4e2c2a8682cbfcfe242e
csucc-cloud <sialongodavie68@gmail.com>
2026-04-17 11:31:30 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,28 @@
-// src/pages/_app.js
-import Layout from '../components/Layout'; // Adjust this path if your Layout is elsewhere
-import '../styles/globals.css';
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Define which paths should NOT have the sidebar/layout
+  const noLayoutPages = ["/login", "/auth/register", "/auth/faculty-enroll"];
+
+  // Check if the current path is in our "no layout" list
+  const isNoLayoutPage = noLayoutPages.includes(router.pathname);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {isNoLayoutPage ? (
+        // If it's a login/auth page, just show the component alone
+        <Component {...pageProps} />
+      ) : (
+        // Otherwise, wrap it in the persistent Layout
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
   );
 }
 
