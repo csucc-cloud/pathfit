08e5b4a71a2efff540dde86622981cb3757cc30d
csucc-cloud <sialongodavie68@gmail.com>
2026-04-17 11:17:33 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 // src/pages/_app.js
-import '../styles/globals.css'; // Make sure you have this file with Tailwind directives
+import Layout from '../components/Layout'; // Adjust this path if your Layout is elsewhere
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
